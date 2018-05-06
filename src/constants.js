5b19d59fde706b19cd831e3bacd92f13dd58570f
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2018-05-06 14:36:27 -0300

    Add constants file

---
+++ src/constants.js
@@ -0,0 +1,25 @@
+export const OPTIONS = {
+  title: {
+    complement: window.document.title,
+    separator: '|'
+  },
+  attr: 'data-head'
+}
+
+export const SHORTHANDS = {
+  ch: 'charset',
+  tg: 'target',
+  n: 'name',
+  he: 'http-equiv',
+  ip: 'itemprop',
+  c: 'content',
+  p: 'property',
+  sc: 'scheme',
+  r: 'rel',
+  h: 'href',
+  sz: 'sizes',
+  t: 'type',
+  s: 'src',
+  a: 'async',
+  d: 'defer'
+}
