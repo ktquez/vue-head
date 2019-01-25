eaf8a8db7875308fdb90d48c5a6aec4ad2b4991e
NateScarlet <NateScarlet@Gmail.com>
2019-01-25 10:32:38 +0800

    Move options interface out of vue namespace

--- vue-head.d.ts
+++ vue-head.d.ts
@@ -1,20 +1,19 @@
 import Vue, { PluginObject } from 'vue';
 declare const VueHead: PluginObject<{}>;
 export = VueHead;
-
+interface TitleOptions {
+  inner?: string;
+  separator?: string;
+  complement?: string;
+}
+interface MetaOptions {
+  itemprop?: string;
+  property?: string;
+  content?: string;
+  id?: string;
+  name?: string;
+}
 declare module 'vue/types/options' {
-  interface TitleOptions {
-    inner?: string;
-    separator?: string;
-    complement?: string;
-  }
-  interface MetaOptions {
-    itemprop?: string;
-    property?: string;
-    content?: string;
-    id?: string;
-    name?: string;
-  }
   interface ComponentOptions<V extends Vue> {
     head?: {
       title?: TitleOptions | (() => TitleOptions);
