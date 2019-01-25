85c13829d44bdf823a48b9549d218b791270e9e2
NateScarlet <NateScarlet@Gmail.com>
2019-01-25 10:47:47 +0800

    Enhance type definitions

--- vue-head.d.ts
+++ vue-head.d.ts
@@ -6,21 +6,74 @@ interface TitleOptions {
   separator?: string;
   complement?: string;
 }
-interface MetaOptions {
-  itemprop?: string;
-  property?: string;
-  content?: string;
-  id?: string;
+interface IElement extends Object {
+  /** Shorthand of `charset` */
+  ch?: string;
+  charset?: string;
+  /** Shorthand of `target` */
+  tg?: string;
+  target?: string;
+  /** Shorthand of `name` */
+  n?: string;
   name?: string;
+  /** Shorthand of `http-equiv` */
+  he?: string;
+  'http-equiv'?: string;
+  /** Shorthand of `itemprop` */
+  ip?: string;
+  itemprop?: string;
+  /** Shorthand of `content` */
+  c?: string;
+  content?: string;
+  /** Shorthand of `property` */
+  p?: string;
+  property?: string;
+  /** Shorthand of `scheme` */
+  sc?: string;
+  scheme?: string;
+  /** Shorthand of `rel` */
+  r?: string;
+  rel?: string;
+  /** Shorthand of `href` */
+  h?: string;
+  href?: string;
+  /** Shorthand of `sizes` */
+  sz?: string;
+  sizes?: string;
+  /** Shorthand of `type` */
+  t?: string;
+  type?: string;
+  /** Shorthand of `src` */
+  s?: string;
+  src?: string;
+  /** Shorthand of `async` */
+  a?: string;
+  async?: string;
+  /** Shorthand of `defer` */
+  d?: string;
+  defer?: string;
+  /** Shorthand of `inner` */
+  i?: string;
+  inner?: string;
+  /**
+   * Option for undo tag when leave the component
+   * https://github.com/ktquez/vue-head#undo-elements
+   */
+  undo?: boolean;
+  /**
+   *  Id for element updating
+   * https://github.com/ktquez/vue-head#replace-content-the-elements
+   */
+  id?: string;
 }
 declare module 'vue/types/options' {
   interface ComponentOptions<V extends Vue> {
     head?: {
       title?: TitleOptions | (() => TitleOptions);
-      meta?: MetaOptions[] | (() => MetaOptions[]);
-      link?: any[];
-      script?: any[];
-      style?: any[];
+      meta?: IElement[] | (() => IElement[]);
+      link?: IElement[] | (() => IElement[]);
+      script?: IElement[] | (() => IElement[]);
+      style?: IElement[] | (() => IElement[]);
     };
   }
 }
